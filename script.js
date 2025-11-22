d6f695322575820901cb280ade1a1d589df3c230
pankajcoder28 <pankajpenu147@gmail.com>
2025-11-22 20:14:15 +0530

    piano

---
+++ script.js
@@ -0,0 +1,351 @@
+var n1 = document.querySelector('#n1')
+var b1 = document.getElementById('b1')
+
+document.body.addEventListener('keyup',(dets)=>{
+    if(dets.key == 'q'){
+        var aud28 = new Audio('./28.mp3')
+        aud28.play()
+    }
+    if(dets.key == 'w'){
+        var aud29 = new Audio('./29.mp3')
+        aud29.play()
+    }
+    if(dets.key == 'e'){
+        var aud30 = new Audio('./30.mp3')
+        aud30.play()
+    }
+    if(dets.key == 'r'){
+        var aud31 = new Audio('./31.mp3')
+        aud31.play()
+    }
+    if(dets.key == 't'){
+        var aud32 = new Audio('./32.mp3')
+        aud32.play()
+    }
+    if(dets.key == 'y'){
+        var aud33 = new Audio('./33.mp3')
+        aud33.play()
+    } 
+    if(dets.key == 'u'){
+        var aud34 = new Audio('./34.mp3')
+        aud34.play()
+    }
+    if(dets.key == 'i'){
+        var aud35 = new Audio('./35.mp3')
+        aud35.play()
+    }
+    if(dets.key == 'o'){
+        var aud36 = new Audio('./36.mp3')
+        aud36.play()
+    }
+    if(dets.key == 'p'){
+        var aud37 = new Audio('./37.mp3')
+        aud37.play()
+    }
+    if(dets.key == 'a'){
+        var aud38 = new Audio('./38.mp3')
+        aud38.play()
+    }
+    if(dets.key == 's'){
+        var aud39 = new Audio('./39.mp3')
+        aud39.play()
+    }
+    if(dets.key == 'd'){
+        var aud40 = new Audio('./40.mp3')
+        aud40.play()
+    }
+    if(dets.key == 'f'){
+        var aud41 = new Audio('./41.mp3')
+        aud41.play()
+    }
+    if(dets.key == 'g'){
+        var aud42 = new Audio('./42.mp3')
+        aud42.play()
+    }
+    if(dets.key == 'h'){
+        var aud43 = new Audio('./43.mp3')
+        aud43.play()
+    }
+    if(dets.key == 'j'){
+        var aud44 = new Audio('./44.mp3')
+        aud44.play()
+    }
+    if(dets.key == 'k'){
+        var aud45 = new Audio('./45.mp3')
+        aud45.play()
+    }
+    if(dets.key == 'l'){
+        var aud46 = new Audio('./46.mp3')
+        aud46.play()
+    }
+    if(dets.key == 'z'){
+        var aud47 = new Audio('./47.mp3')
+        aud47.play()
+    }
+    if(dets.key == 'x'){
+        var aud48 = new Audio('./48.mp3')
+        aud48.play()
+    }
+    if(dets.key == 'c'){
+        var aud49 = new Audio('./49.mp3')
+        aud49.play()
+    }
+    if(dets.key == 'v'){
+        var aud50 = new Audio('./50.mp3')
+        aud50.play()
+    }
+    if(dets.key == 'b'){
+        var aud51 = new Audio('./51.mp3')
+        aud51.play()
+    }
+    if(dets.key == 'n'){
+        var aud52 = new Audio('./52.mp3')
+        aud52.play()
+    }
+    if(dets.key == 'm'){
+        var aud53 = new Audio('./53.mp3')
+        aud53.play()
+    }
+    if(dets.key == ','){
+        var aud54 = new Audio('./54.mp3')
+        aud54.play()
+    }
+
+    if(dets.key == '.'){
+        var aud55 = new Audio('./55.mp3')
+        aud55.play()
+    }
+    if(dets.key == '/'){
+        var aud56 = new Audio('./56.mp3')
+        aud56.play()
+    }
+    if(dets.key == '['){
+        var aud57 = new Audio('./57.mp3')
+        aud57.play()
+    }
+    if(dets.key == ']'){
+        var aud58 = new Audio('./58.mp3')
+        aud58.play()
+    }
+    if(dets.key == '1'){
+        var aud59 = new Audio('./59.mp3')
+        aud59.play()
+    }
+    if(dets.key == '2'){
+        var aud60 = new Audio('./60.mp3')
+        aud60.play()
+    }
+    if(dets.key == '3'){
+        var aud61 = new Audio('./61.mp3')
+        aud61.play()
+    }
+    if(dets.key == '4'){
+        var aud62 = new Audio('./62.mp3')
+        aud62.play()
+    }
+    if(dets.key == '5'){
+        var aud63 = new Audio('./63.mp3')
+        aud63.play()
+    }
+
+})
+ n1.addEventListener('click',()=>{
+    var aud28 = new Audio('./28.mp3')
+    aud28.play()   
+})
+b1.addEventListener('click',()=>{
+    var aud29 = new Audio('./29.mp3')
+    aud29.play()
+})
+
+var n2 = document.getElementById('n2')
+var b2 = document.getElementById('b2')
+
+n2.addEventListener('click',()=>{
+    var aud30 = new Audio('./30.mp3')
+    aud30.play()
+})
+b2.addEventListener('click',()=>{
+    var aud31 = new Audio('./31.mp3')
+        aud31.play()
+})
+
+var n3 = document.getElementById('n3')
+n3.addEventListener('click',()=>{
+    var aud32 = new Audio('./32.mp3')
+    aud32.play()
+})
+
+var n4 = document.getElementById('n4')
+n4.addEventListener('click',()=>{
+    var aud33 = new Audio('./33.mp3')
+    aud33.play()
+})
+
+var b3 = document.getElementById('b3')
+b3.addEventListener('click',()=>{
+    var aud34 = new Audio('./34.mp3')
+    aud34.play()
+})
+
+var n5 = document.getElementById('n5')
+var b4 = document.getElementById('b4')
+
+n5.addEventListener('click',()=>{
+    var aud35 = new Audio('./35.mp3')
+    aud35.play()
+})
+b4.addEventListener('click',()=>{
+    var aud36 = new Audio('./36.mp3')
+    aud36.play()
+})
+
+var n6 = document.getElementById('n6')
+var b5 = document.getElementById('b5')
+n6.addEventListener('click',()=>{
+    var aud37 = new Audio('./37.mp3')
+    aud37.play()
+})
+b5.addEventListener('click',()=>{
+    var aud38 = new Audio('./38.mp3')
+    aud38.play()
+})
+
+var n7 = document.getElementById('n7')
+var n8 = document.getElementById('n8')
+var b6 = document.addEventListener('b6')
+n7.addEventListener('click',()=>{
+     var aud39 = new Audio('./39.mp3')
+    aud39.play()
+})
+n8.addEventListener('click',()=>{
+    var aud40 = new Audio('./40.mp3')
+    aud40.play()
+})
+b6.addEventListener('click',()=>{
+    var aud41 = new Audio('./41.mp3')
+    aud41.play()
+})
+
+var n9 = document.getElementById('n9')
+var b7 = document.addEventListener('b7')
+n9.addEventListener('click',()=>{
+    var aud42 = new Audio('./42.mp3')
+    aud42.play()
+})
+b7.addEventListener('click',()=>{
+    var aud43 = new Audio('./43.mp3')
+    aud43.play()
+})
+
+var n10 = document.getElementById('n10')
+var n11 = document.getElementById('n11')
+var b8 = document.getElementById('b8')
+n10.addEventListener('click',()=>{
+    var aud44 = new Audio('./44.mp3')
+    aud44.play()
+})
+n11.addEventListener('click',()=>{
+    var aud45 = new Audio('./45.mp3')
+    aud45.play()
+})
+b8.addEventListener('click',()=>{
+    var aud45 = new Audio('./46.mp3')
+    aud45.play()
+})
+var n12 = document.getElementById('n12')
+var b9 = document.getElementById('b9')
+n12.addEventListener('click',()=>{
+    var aud47 = new Audio('./47.mp3')
+    aud47.play()
+})
+b9.addEventListener('click',()=>{
+    var aud48 = new Audio('./48.mp3')
+    aud48.play()
+})
+
+var n13 = document.getElementById('n13')
+var b10 = document.getElementById('b10')
+n13.addEventListener('click',()=>{
+    var aud49 = new Audio('./49.mp3')
+    aud49.play()
+})
+b10.addEventListener('click',()=>{
+    var aud50 = new Audio('./50.mp3')
+    aud50.play()
+})
+
+var n14 = document.getElementById('n14')
+var b11 = document.getElementById('b11')
+n14.addEventListener('click',()=>{
+    var aud51 = new Audio('./51.mp3')
+    aud51.play()
+})
+b11.addEventListener('click',()=>{
+    var aud52 = new Audio('./52.mp3')
+    aud52.play()
+})
+
+var n15 = document.getElementById('n15')
+var n16 = document.getElementById('n16')
+n15.addEventListener('click',()=>{
+    var aud53 = new Audio('./53.mp3')
+    aud53.play()
+})
+n16.addEventListener('click',()=>{
+    var aud54 = new Audio('./54.mp3')
+    aud54.play()
+})
+
+var b12 = document.getElementById('b12')
+var n17 = document.getElementById('n17')
+b12.addEventListener('click',()=>{
+    var aud55 = new Audio('./55.mp3')
+    aud55.play()
+})
+n17.addEventListener('click',()=>{
+    var aud56 = new Audio('./56.mp3')
+    aud56.play()
+})
+
+var n18 = document.getElementById('n18')
+var b13 = document.getElementById('b13')
+n18.addEventListener('click',()=>{
+    var aud57 = new Audio('./57.mp3')
+    aud57.play()
+})
+b13.addEventListener('click',()=>{
+    var aud58 = new Audio('./58.mp3')
+    aud58.play()
+})
+
+var n19 = document.getElementById('n19')
+var b14 = document.getElementById('b14')
+n19.addEventListener('click',()=>{
+    var aud59 = new Audio('./59.mp3')
+    aud59.play()
+})
+b14.addEventListener('click',()=>{
+    var aud60 = new Audio('./60.mp3')
+    aud60.play()
+})
+
+var n20 = document.getElementById('n20')
+var b15 = document.getElementById('b15')
+n20.addEventListener('click',()=>{
+    var aud61 = new Audio('./61.mp3')
+    aud59.play()
+})
+b15.addEventListener('click',()=>{
+    var aud62 = new Audio('./62.mp3')
+    aud62.play()
+})
+
+var n21 = document.getElementById('n21')
+n21.addEventListener('click',()=>{
+    var aud63 = new Audio('./63.mp3')
+    aud63.play()
+})
+
+
+
